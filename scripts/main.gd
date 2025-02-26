a2703fad380f6638ef9a13e6588bca74c6889989
luizing <luizeduardo2099@gmail.com>
2025-02-26 02:19:39 -0300

    refactor

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,66 +2,86 @@ extends Node
 
 @export var pontuacao_maxima = 1
 @export var tempo_maximo = 10
+@export var varVelocidadeRapida = Vector2(700,720)
+@export var varVelocidadeLenta = Vector2(300,320)
 
 var cena_carros = preload("res://Cenas/carros.tscn")
 
-var pistas_rapidas_y = [104, 272, 488]
-var pistas_lentas_y = [160, 216, 324, 384, 438, 544, 600]
-var pistas_invertidas_y = [160, 324, 388, 544]
+var pistas_y = [104, 160, 216, 272, 324, 384, 438, 488, 544, 600]
+
+#Randomizar?
+var pistas_rapidas_index = [0, 4, 6, 7]
+var pistas_rapidas_y = []
+var pistas_lentas_y = []
+var pistas_invertidas_index = [1, 5, 7, 9]
+var pistas_invertidas_y = []
 
 var cronometro = 0
-
 var score = 0
 
 func reset() -> void:
 	score = 0
-	$HUD/Placar.text = str(score)
+	cronometro = 0
 	$HUD/Mensagem.hide()
 	$HUD/Button.hide()
+	$LabelPlacar.text = str(score)
+	
 	$AudioTema.play()
+	
+	$Player.reset()
+	
+	$TimerGameOver.start()
+	$TimerCarrosLentos.start()
+	$TimerCarrosRapidos.start()
+	
+func spawn_carro(pistas_lista: Array, velocidade_min: float, velocidade_max: float) -> void:
+	var carro = cena_carros.instantiate()
+	add_child(carro)
+	
+	var pista_y = pistas_lista[randi() % pistas_lista.size()]
+	
+	if pista_y in pistas_invertidas_y:
+		carro.position = Vector2(+1300, pista_y)
+		carro.set_linear_velocity(Vector2(randf_range(-velocidade_max, -velocidade_min), 0))
+	else:
+		carro.position = Vector2(-10, pista_y)
+		carro.set_linear_velocity(Vector2(randf_range(velocidade_min, velocidade_max), 0))
+	
+	carro.set_linear_damp(0.0)
 
-
+func setup_pistas() -> void:
+	for index in range(pistas_y.size()):
+		if pistas_rapidas_index.has(index):
+			pistas_rapidas_y.append(pistas_y[index])
+		else:
+			pistas_lentas_y.append(pistas_y[index])
+	
+	for index in pistas_invertidas_index:
+		if index >= 0 and index < pistas_y.size():
+			pistas_invertidas_y.append(pistas_y[index])
+	
 func _ready() -> void:
 	reset()
+	setup_pistas()
 	randomize()
 
 func _on_timer_carros_rapidos_timeout() -> void:
-	var carro = cena_carros.instantiate()
-	add_child(carro)
-	
-	var pista_y = pistas_rapidas_y[randi_range(0, pistas_rapidas_y.size() - 1)]
-	
-	
-	if pistas_invertidas_y.has(pista_y):
-		carro.position = Vector2(-10, pista_y)
-		carro.set_linear_velocity(Vector2(randf_range(700, 720), 0))
-
-	else:
-		carro.position = Vector2(+1300, pista_y)
-		carro.set_linear_velocity(Vector2(randf_range(-650, -720), 0))
-	
-	carro.set_linear_damp(0.0)
+	spawn_carro(pistas_rapidas_y, varVelocidadeRapida.x, varVelocidadeRapida.y)
 	
 func _on_timer_carros_lentos_timeout() -> void:
-	var carro = cena_carros.instantiate()
-	add_child(carro)
+	spawn_carro(pistas_lentas_y, varVelocidadeLenta.x, varVelocidadeLenta.y)
 	
-	var pista_y = pistas_lentas_y[randi_range(0, pistas_lentas_y.size() - 1)]
-	
-	carro.position = Vector2(-10, pista_y)
-	carro.set_linear_velocity(Vector2(randf_range(300, 320), 0))
-	carro.set_linear_damp(0.0)
-
 func _on_player_pontua() -> void:	
 	if score <= pontuacao_maxima:
 		score += 1
-		$HUD/Placar.text = str(score)
+		$LabelPlacar.text = str(score)
 		$AudioPonto.play()
 		$Player.position = $Player.posicao_inicial
 	
 	if score == pontuacao_maxima:
-		$HUD/Placar.text = str(score)
+		$LabelPlacar.text = str(score)
 		$HUD/Mensagem.text = "Parabens voce venceu"
+		$TimerGameOver.stop()
 		$HUD/Button.show()
 		$AudioVitoria.play()
 		
@@ -71,25 +91,16 @@ func _on_player_pontua() -> void:
 		$Player.speed = 0
 		
 func _on_hud_reinicia() -> void:
-	score = 0
-	$HUD/Placar.text = str(score)
-	$HUD/Mensagem.text = ""
-	$HUD/Button.hide()
-	$AudioTema.play()
-	
 	reset()
-	$Player.speed = $Player.PlayerSpeed
-	$TimerCarrosLentos.start()
-	$TimerCarrosRapidos.start()
-
 
 func _on_timer_game_over_timeout():
 	cronometro += 1
 	$LabelTimer.text = "Timer: " + str(cronometro)
 	if cronometro == tempo_maximo:
 		#GameOver
-		reset()
-		$Player.position = $Player.posicao_inicial
-		cronometro = 0
 		$LabelTimer.text = "TimeOut"
-		
+		reset()
+
+func _on_player_morre() -> void:
+	reset()
+	
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,10 +1,13 @@
 extends Area2D
 
 signal pontua
+signal morre
+
 @export var PlayerSpeed: float = 500.0
 var speed = PlayerSpeed
 
 var screen_size: Vector2
+
 var posicao_inicial: Vector2 = Vector2(640, 690)
 
 func _ready() -> void:
@@ -23,7 +26,6 @@ func _process(delta: float) -> void:
 		velocity.x -= 1
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
-	
 		
 	if velocity != Vector2.ZERO:
 		velocity = velocity.normalized() * speed
@@ -39,11 +41,15 @@ func _process(delta: float) -> void:
 	else:
 		$Animacao.stop()
 
-
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "LinhaChegada":
 		emit_signal("pontua")
 	else:
-		$Audio.play()
-		position = posicao_inicial
+		emit_signal("morre")
+		$AudioMorte.play()
+		
+func reset() -> void:
+	position = posicao_inicial
+	speed = PlayerSpeed
+	
 	
